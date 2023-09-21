abd579e0e168db3b207839ce6f3a438cd732c6d0
fiademm <fiademm1@gmail.com>
2023-09-21 13:49:05 +0000

    Update products component

--- src/app/other/components/Products.js
+++ src/app/other/components/Products.js
@@ -1,5 +1,8 @@
 import React, { useState } from 'react'
 import Header from '../components/Header';
+import { ToastContainer, toast } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
+import { ProductsCardList } from './CardsList';
 
 const ViewProducts = () => {
     const [showViewProduct, setShowViewProduct] = useState(true);
@@ -8,6 +11,69 @@ const ViewProducts = () => {
         setShowViewProduct(!showViewProduct);
     };
 
+    const products = [
+        {
+            id: 1,
+            barcode: 123456789012,
+            name: "<Brand> <Name> <Size> <Color>",
+            category: 'Category',
+            price: 267123456.52,
+            quantity: 10,
+            reOrderValue: 5,
+            status: 'Available'
+        },
+        {
+            id: 2,
+            barcode: 123456789012,
+            name: "<Brand> <Name> <Size> <Color>",
+            category: 'Category',
+            price: 2.50,
+            quantity: 10,
+            reOrderValue: 5,
+            status: 'Available'
+        },
+        {
+            id: 3,
+            barcode: 123456789012,
+            name: "<Brand> <Name> <Size> <Color>",
+            category: 'Category',
+            price: 2.50,
+            quantity: 10,
+            reOrderValue: 5,
+            status: 'Reorder'
+        },
+        {
+            id: 4,
+            barcode: 123456789012,
+            name: "<Brand> <Name> <Size> <Color>",
+            category: 'Category',
+            price: 267123456.52,
+            quantity: 10,
+            reOrderValue: 5,
+            status: 'Reorder'
+        },
+        {
+            id: 5,
+            barcode: 123456789012,
+            name: "<Brand> <Name> <Size> <Color>",
+            category: 'Category',
+            price: 2.50,
+            quantity: 10,
+            reOrderValue: 5,
+            status: 'Available'
+        },
+        {
+            id: 6,
+            barcode: 123456789012,
+            name: "<Brand> <Name> <Size> <Color>",
+            category: 'Category',
+            price: 532000.50,
+            quantity: 10,
+            reOrderValue: 5,
+            status: 'Out of stock'
+        },
+    ]
+
     return (
         <>
             {showViewProduct ? (
@@ -15,7 +81,9 @@ const ViewProducts = () => {
                     <Header title='View products' route1='Create product' toggle={handleScreenToggle} />
 
                     <section className="content">
-                        View Products
+                        <div className="view-container">
+                            <ProductsCardList products={products} />
+                        </div>
                     </section>
                 </section>
             ) : (
@@ -27,10 +95,38 @@ const ViewProducts = () => {
 
 const CreateProduct = () => {
     const [showViewProduct, setShowViewProduct] = useState(false);
+    const [barcode, setBarcode] = useState();
+    const [name, setName] = useState('');
+    const [category, setCategory] = useState('');
+    const [price, setPrice] = useState();
+    const [quantity, setQuantity] = useState();
+    const [reOrderValue, setReordervalue] = useState();
+    const [status, setStatus] = useState('');
 
     const handleScreenToggle = () => {
         setShowViewProduct(!showViewProduct);
     };
+
+    const handleValidation = () => {
+        if (barcode === '' || name === '' || category === '' || price === '' || quantity === '' || reOrderValue === '' || status === '' ) {
+            toast.error('All fields are required.')
+            return false;
+        } else {
+            return true;
+        }
+    }
+
+    const handleSave = () => {
+        handleValidation();
+
+        if (handleValidation() === true) {
+            try {
+                toast.success('Record added successfully.')
+            } catch (error) {
+                toast.error('Error adding record.')
+            }
+        }
+    }
 
     return (
         <>
