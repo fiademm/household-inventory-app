784bb72f3dda0700d114f95c9b5fcd5362f984a1
fiademm <fiademm1@gmail.com>
2023-09-22 08:42:34 +0000

    Update transactions cardlist

--- src/app/other/components/CardsList.js
+++ src/app/other/components/CardsList.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { CategoriesCard, ProductsCard, UsersCard } from './Cards';
+import { CategoriesCard, ProductsCard, TransactionsCard, UsersCard } from './Cards';
 
 const CategoriesCardList = ({ categories }) => {
     return (
@@ -80,9 +80,32 @@ const SettingsCardList = () => {
     );
 };
 
-const TransactionsCardList = () => {
+const TransactionsCardList = ({ transactions }) => {
     return (
-        <></>
+        <>
+            <section className="card-deck">
+                <div className="custom-card-1 category-card card-headings">
+                    <span style={{ textAlign: 'center', width: '5%', fontFamily: 'Jost-Medium' }}>Index</span>
+                    <span style={{ textAlign: 'left', width: '42%' }}>Product name</span>
+                    <span style={{ textAlign: 'left', width: '10%' }}>Price</span>
+                    <span style={{ textAlign: 'left', width: '10%', textAlign: 'center' }}>Quantity</span>
+                    <span style={{ textAlign: 'left', width: '10%' }}>Amount</span>
+                    <span style={{ textAlign: 'left', width: '25%' }}>Date</span>
+                </div>
+                {transactions.map((transaction, index) => (
+                    <TransactionsCard
+                        key={transaction.id}
+                        index={index + 1}
+                        id={transaction.id}
+                        product={transaction.product}
+                        price={transaction.price}
+                        quantity={transaction.quantity}
+                        amount={transaction.amount}
+                        date={transaction.date}
+                    />
+                ))}
+            </section>
+        </>
     );
 };
 
